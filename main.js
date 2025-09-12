bc13ee016f9b57325cb70c8052d19a747a6cb471
analiria <pires.portes@escola.pr.gov.br>
2025-09-12 09:24:30 -0300

    checkbox

--- main.js
+++ main.js
@@ -27,14 +27,33 @@ function aumentatamanho(){
 const camposenha = document.querySelector("#campo-senha");
 
 const letramaiusculas = "ABCDEFGHIJKLMNOPQRSTUVXWZ";
+const letraminusculas = "abcdefghijklmnopqrstuvxwz";
+const numero = "0123456789"
+const simbolos = "!@%*?";
+
 gerasenha();
 
 function gerasenha(){
+    let alfabeto ='';
+    if (checkbox[0].checked){ 
+        alfabeto = alfabeto + letramaiusculas;
+    }
+    if (checkbox[0].checked){ 
+        alfabeto = alfabeto + letraminusculas;
+    }
+    if (checkbox[0].checked){ 
+        alfabeto = alfabeto + numero;
+    }
+    if (checkbox[0].checked){ 
+        alfabeto = alfabeto + simbolos;
+    }
+    
     let senha = '';
     for(let i = 0; i <tamanhosenha; i++){
         let numeroaleatorio = Math.random()* letramaiusculas.length;
         numeroaleatorio = Math.floor(numeroaleatorio);
-        senha = senha + letramaiusculas[numeroaleatorio];
+        senha = senha + alfabeto[numeroaleatorio];
     }
     camposenha.value = senha;
+
         
